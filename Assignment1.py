55985f576d524e3f432fae389156ab5b4776407b
Lata Sharma <vanshusharma4045@gmail.com>
2024-08-05 21:06:57 +0530

    Create Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,33 @@
+# # 1.GIVE OUTPUT OF THE FOLLOWING CODE
+# x,y=7,2
+# x,y,x=x+1,y+3,x+10
+# print(x,y)
+
+# # 17,5
+
+# # 2. GIVE OUTPUT OF THE FOLLOWING CODE
+# x,y=20,30
+# y,y=x+10,x+20
+# print(x,y)
+# # 20,40
+
+# # 3. Find output
+
+# x,y=10,100
+# y=x/2
+# x='day'
+# y=x/2
+# # error
+
+# 4. Calculate distance price of petrol from your home to Nielit (petrol
+# price=97.48). And then perform explicit type conversion to convert to
+# float result into integer.
+distance=7.6 
+petrol=97.48
+cost=int(distance*petrol)
+print(cost)
+
+
+# print all keywords
+import keyword
+print(keyword.kwlist)
