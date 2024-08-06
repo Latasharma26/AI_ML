4e9e0ce4a813f2dcf73dd1475c915af4c587e2bd
Lata Sharma <vanshusharma4045@gmail.com>
2024-08-06 19:55:48 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,23 +1,23 @@
-# # 1.GIVE OUTPUT OF THE FOLLOWING CODE
-# x,y=7,2
-# x,y,x=x+1,y+3,x+10
-# print(x,y)
+# 1.GIVE OUTPUT OF THE FOLLOWING CODE
+x,y=7,2
+x,y,x=x+1,y+3,x+10
+print(x,y)
 
-# # 17,5
+# 17,5
 
-# # 2. GIVE OUTPUT OF THE FOLLOWING CODE
-# x,y=20,30
-# y,y=x+10,x+20
-# print(x,y)
-# # 20,40
+# 2. GIVE OUTPUT OF THE FOLLOWING CODE
+x,y=20,30
+y,y=x+10,x+20
+print(x,y)
+# 20,40
 
-# # 3. Find output
+# 3. Find output
 
 # x,y=10,100
 # y=x/2
 # x='day'
 # y=x/2
-# # error
+# error
 
 # 4. Calculate distance price of petrol from your home to Nielit (petrol
 # price=97.48). And then perform explicit type conversion to convert to
@@ -26,6 +26,10 @@ distance=7.6
 petrol=97.48
 cost=int(distance*petrol)
 print(cost)
+
+
+# 5 Different types of errors (run time; compile time, syntax error, and
+# indentation error)
 
 # Syntax Error(compile time error)
 # print("hello"
@@ -40,7 +44,7 @@ print(cost)
       
 
 
-# WAP to show escape sequences.
+# 6.WAP to show escape sequences.
 
 #new line \n
 print("new \n line ")
@@ -59,6 +63,6 @@ print("hello\b")
 
 
 
-# print all keywords
+# 7.print all keywords
 import keyword
 print(keyword.kwlist)
